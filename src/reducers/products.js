8b38c26315dd97305c115467fba2eaf563850efb
Stelios Voskos <s.voskos@googlemail.com>
2020-01-20 08:27:39 +0000

    changed images and titles

--- src/reducers/products.js
+++ src/reducers/products.js
@@ -1,4 +1,12 @@
 import * as actionType from '../actions/actionTypes';
+import blondeRoast from './assets/blondeRoast.jpg';
+import darkRoast from './assets/darkRoast.jpg';
+import mediumRoast from './assets/mediumRoast.jpg';
+import arabica from './assets/arabica.jpg';
+import coffeeMachine from './assets/coffeeMachine.jpg';
+import espressoMaker from './assets/espressoMaker.jpg';
+import latte from './assets/latte.jpg';
+import flatWhite from './assets/flatWhite.jpg';
 
 const initialState = {
     basket: [],
@@ -7,6 +15,7 @@ const initialState = {
     totalPrice: 0
 };
 
+
 const products = (state = initialState, action) => {
     switch (action.type) {
         case actionType.LOAD_PRODUCTS:
@@ -14,153 +23,123 @@ const products = (state = initialState, action) => {
                 productList: [
                     {
                         id: 1,
-                        image: "https://picsum.photos/215/200",
-                        title: "Carrots",
-                        description: '1kg of quality Carrots',
+                        image: blondeRoast,
+                        title: "Colombian blonde roast",
+                        description: '500g of grade A Colombian blonde roast.',
+                        rating: 4.5,
+                        isCl: false,
+                        isKg: true,
+                        isVegan: true,
+                        unit: "kg",
+                        isFeatured: true,
+                        isOffer: true,
+                        price: 7,
+                        newPrice: 6.2
+                    },
+                    {
+                        id: 2,
+                        image: darkRoast,
+                        title: "Egyptian dark roast",
+                        description: '500g of grade A Egyptian dark roast.',
+                        rating: 4.3,
+                        isCl: false,
+                        isKg: true,
+                        isVegan: false,
+                        unit: "kg",
+                        isFeatured: false,
+                        isOffer: true,
+                        price: 8.5,
+                        newPrice: 8.0
+                    },
+                    {
+                        id: 3,
+                        image: mediumRoast,
+                        title: "500g Arabica medium roast",
+                        description: '500g of grade A Arabica medium roast.',
                         rating: 4.8,
                         isCl: false,
                         isKg: true,
                         isVegan: true,
                         unit: "kg",
                         isFeatured: true,
-                        isOffer: true,
-                        price: 1.5,
-                        newPrice: 1.2
-                    },
-                    {
-                        id: 2,
-                        image: "https://picsum.photos/215/200",
-                        title: "Potatoes",
-                        description: '500g of quality Potatoes. Country of origin: Cyprus',
-                        rating: 4.7,
-                        isCl: false,
-                        isKg: true,
-                        isVegan: false,
-                        unit: "kg",
-                        isFeatured: false,
-                        isOffer: true,
-                        price: 4.5,
-                        newPrice: 4.0
-                    },
-                    {
-                        id: 3,
-                        image: "https://picsum.photos/215/200",
-                        title: "Sirloin steak",
-                        description: '350kg of 30 day mature sirloin steak',
-                        rating: 4.6,
-                        isCl: false,
-                        isKg: true,
-                        isVegan: true,
-                        unit: "kg",
-                        isFeatured: true,
                         isOffer: false,
-                        price: 3.5,
+                        price: 8.5,
                         newPrice: 0
                     },
                     {
                         id: 4,
-                        image: "https://picsum.photos/215/200",
-                        title: "onions",
-                        description: '500g of quality onions. Country of origin: Cyprus',
-                        rating: 4.7,
-                        isCl: false,
-                        isKg: true,
-                        isVegan: true,
-                        unit: "kg",
-                        isFeatured: false,
-                        isOffer: true,
-                        price: 1.5,
-                        newPrice: 1.
-                    },
-                    {
-                        id: 5,
-                        image: "https://picsum.photos/215/200",
-                        title: "Turkey",
-                        description: '750kg of quality turkey',
-                        rating: 4.9,
-                        isCl: false,
-                        isKg: true,
-                        isVegan: false,
-                        unit: "kg",
-                        isFeatured: false,
-                        isOffer: true,
-                        price: 9.7,
-                        newPrice: 9
-                    },
-                    {
-                        id: 6,
-                        image: "https://picsum.photos/215/200",
-                        title: "Plum tomatoes",
-                        description: '500g of quality plum tomatoes. Country of origin: Cyprus',
+                        image: arabica,
+                        title: "850g Arabica medium roast",
+                        description: '850g of quality onions. Country of origin: Cyprus',
                         rating: 4.3,
                         isCl: false,
                         isKg: true,
                         isVegan: true,
                         unit: "kg",
                         isFeatured: false,
+                        isOffer: true,
+                        price: 8.5,
+                        newPrice: 8.
+                    },
+                    {
+                        id: 5,
+                        image: coffeeMachine,
+                        title: "Coffee pod machine",
+                        description: 'High quality and precision coffee machine. It accepts all coffee pods',
+                        rating: 4.9,
+                        isCl: false,
+                        isKg: true,
+                        isVegan: false,
+                        unit: "kg",
+                        isFeatured: false,
+                        isOffer: true,
+                        price: 90,
+                        newPrice: 70
+                    },
+                    {
+                        id: 6,
+                        image: espressoMaker,
+                        title: "Italian expresso maker",
+                        description: 'One of the best coffee machines for making espresso',
+                        rating: 4.8,
+                        isCl: false,
+                        isKg: true,
+                        isVegan: true,
+                        unit: "kg",
+                        isFeatured: false,
                         isOffer: false,
-                        price: 2.5,
+                        price: 10.5,
                         newPrice: 0.
                     },
                     {
                         id: 7,
-                        image: "https://picsum.photos/215/200",
-                        title: "Potatoes",
-                        description: '500g of quality Potatoes. Country of origin: Cyprus',
+                        image: latte,
+                        title: "Latte",
+                        description: 'Come and enjoy our series of lattes at one of our stores.',
                         rating: 4.7,
                         isCl: false,
                         isKg: true,
                         isVegan: false,
                         unit: "kg",
                         isFeatured: false,
-                        isOffer: true,
-                        price: 4.5,
-                        newPrice: 4.0
+                        isOffer: false,
+                        price: 2.5,
+                        newPrice: 0
                     },
                     {
                         id: 8,
-                        image: "https://picsum.photos/215/200",
-                        title: "Pork for souvlaki",
-                        description: '1kg of quality pork for souvlaki. Country of origin: Cyprus',
-                        rating: 4.5,
+                        image: flatWhite,
+                        title: "Flat white",
+                        description: 'Come and enjoy a high quality flat white at one of our stores.',
+                        rating: 4.6,
                         isCl: false,
                         isKg: true,
                         isVegan: false,
                         unit: "kg",
                         isFeatured: false,
                         isOffer: true,
-                        price: 5.5,
-                        newPrice: 5.
-                    },
-                    {
-                        id: 9,
-                        image: "https://picsum.photos/215/200",
-                        title: "Lemonade",
-                        description: '750cl of homemade lemonade',
-                        rating: 4.9,
-                        isCl: false,
-                        isKg: true,
-                        isVegan: true,
-                        unit: "kg",
-                        isFeatured: false,
-                        isOffer: false,
-                        price: 2,
-                        newPrice: 0
-                    },
-                    {
-                        id: 10,
-                        image: "https://picsum.photos/215/200",
-                        title: "Spring onions",
-                        description: '100g of quality plum tomatoes. Country of origin: Cyprus',
-                        rating: 4.9,
-                        isCl: false,
-                        isKg: true,
-                        isVegan: true,
-                        unit: "kg",
-                        isFeatured: false,
-                        isOffer: true,
-                        price: .9,
-                        newPrice: .7
+                        price: 2.3,
+                        newPrice: 2.
                     }
                 ]
             })
